aab5294db536135bc15a44ff5d06d3c143e66dca
Frederic Lemmel <frederic.lemmel@ext.esa.int>
2025-07-28 09:50:56 +0200

    Remove 'print' statements.

--- pyxel/models/photon_collection/simple_collection.py
+++ pyxel/models/photon_collection/simple_collection.py
@@ -557,11 +557,11 @@ def simple_collection(
             dims=["ref", "wavelength"],
             attrs={"units": str(converted_flux_3d.unit)},
         )
-        min_x, max_x = scene_data["x"].min().item(), scene_data["x"].max().item()
-        min_y, max_y = scene_data["y"].min().item(), scene_data["y"].max().item()
-        print(f"Scene x range: {min_x} to {max_x}")
-        print(f"Scene y range: {min_y} to {max_y}")
-        print("Scene attributes:", scene_data.attrs)
+        # min_x, max_x = scene_data["x"].min().item(), scene_data["x"].max().item()
+        # min_y, max_y = scene_data["y"].min().item(), scene_data["y"].max().item()
+        # print(f"Scene x range: {min_x} to {max_x}")
+        # print(f"Scene y range: {min_y} to {max_y}")
+        # print("Scene attributes:", scene_data.attrs)
 
         photon_projected = project_objects_to_detector(
             scene_data=scene_data,
