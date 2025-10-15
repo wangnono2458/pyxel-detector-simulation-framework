38e3e325f278cd79dd13626d99462f1e3a1f1901
Frederic Lemmel <frederic.lemmel@ext.esa.int>
2025-10-15 18:20:56 +0200

    Fix model 'output_node_noise_cmos'.

--- pyxel/models/charge_measurement/output_node_noise_cmos.py
+++ pyxel/models/charge_measurement/output_node_noise_cmos.py
@@ -9,6 +9,7 @@
 """Readout noise model."""
 
 import numpy as np
+from astropy.units import Quantity
 
 from pyxel.detectors import CMOS
 from pyxel.util import set_random_seed
@@ -16,9 +17,9 @@ from pyxel.util import set_random_seed
 
 def create_noise_cmos(
     shape: tuple[int, int],
-    readout_noise: float,
-    readout_noise_std: float,
-    charge_readout_sensitivity: float | np.ndarray,
+    readout_noise: Quantity,
+    readout_noise_std: Quantity,
+    sensitivity_2d: Quantity,
 ) -> np.ndarray:
     """Create noise to signal array for :term:`CMOS` detectors.
 
@@ -30,7 +31,7 @@ def create_noise_cmos(
         The mean readout noise level per pixel.
     readout_noise_std : float
         The standard deviation of the readout noise
-    charge_readout_sensitivity : float | dict[str, float]
+    sensitivity_2d : float
         Charge readout sensitivity could be a scalar or a 2D array.
 
     Returns
@@ -38,24 +39,22 @@ def create_noise_cmos(
     ndarray
         The generated 2D noise array.
     """
-    # Create an array for sensitivities that matches the detector's shape
-    if not isinstance(charge_readout_sensitivity, np.ndarray):
-        sensitivity_2d = np.full(shape=shape, fill_value=charge_readout_sensitivity)
-    else:
-        sensitivity_2d = charge_readout_sensitivity
-
     # Generate the noise based on the calculated sensitivities
-    noise_mean_2d = readout_noise * sensitivity_2d
-    noise_std_2d = readout_noise_std * sensitivity_2d
+    noise_mean_2d: Quantity = readout_noise * sensitivity_2d
+    noise_std_2d: Quantity = readout_noise_std * sensitivity_2d
 
     # Generate the noise with Gaussian distribution
-    sigma_2d = np.random.normal(loc=noise_mean_2d, scale=noise_std_2d, size=shape)
+    sigma_2d = np.random.normal(
+        loc=noise_mean_2d.to("V").value,
+        scale=noise_std_2d.to("V").value,
+        size=shape,
+    )
     sigma_2d = sigma_2d.clip(min=0.0)  # Ensure noise values are non-negative
 
     # TODO: Is it correct ?
     noise_2d = np.random.normal(scale=sigma_2d)
 
-    return noise_2d
+    return Quantity(noise_2d, unit="V")
 
 
 def output_node_noise_cmos(
@@ -90,7 +89,9 @@ def output_node_noise_cmos(
     if readout_noise_std < 0.0:
         raise ValueError("'readout_noise_std' must be positive.")
 
-    charge_readout_sensitivity = detector.characteristics.channels_gain
+    charge_readout_sensitivity = Quantity(
+        detector.characteristics.charge_to_volt_conversion, unit="V/electron"
+    )
     # if isinstance(detector.characteristics.charge_to_volt_conversion, (float, int)):
     #     charge_readout_sensitivity[:] = detector.characteristics.charge_to_volt_conversion
     # elif isinstance(detector.characteristics.charge_to_volt_conversion, dict):
@@ -104,11 +105,11 @@ def output_node_noise_cmos(
     #     )
 
     with set_random_seed(seed):
-        noise_2d: np.ndarray = create_noise_cmos(
+        noise_2d: Quantity = create_noise_cmos(
             shape=detector.signal.shape,
-            readout_noise=readout_noise,
-            readout_noise_std=readout_noise_std,
-            charge_readout_sensitivity=charge_readout_sensitivity,
+            readout_noise=Quantity(readout_noise, unit="electron"),
+            readout_noise_std=Quantity(readout_noise_std, unit="electron"),
+            sensitivity_2d=charge_readout_sensitivity,
         )
 
-    detector.signal.array += noise_2d
+    detector.signal += noise_2d
